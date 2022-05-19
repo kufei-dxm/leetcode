b14918df9ed4701f6f9bdfda8d5a80abf73df223
kufei <kufei.dxm@alibaba-inc.com>
2022-05-20 00:39:08 +0800

    一刷

---
+++ src/main/java/com/leetcode/Problem136.java
@@ -0,0 +1,36 @@
+package com.leetcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author kufei.dxm
+ * @date 2022/5/20
+ */
+public class Problem136 {
+    private Problem136 solution;
+
+    /**
+     * 解题思路：异或操作符。相同为0 ，不同为1.
+     * 参考：https://www.ruanyifeng.com/blog/2021/01/_xor.html
+     *
+     * @param nums
+     * @return
+     */
+    public int singleNumber(int[] nums) {
+        int n = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            n ^= nums[i];
+        }
+        return n;
+    }
+
+    @Test
+    public void test() {
+        solution = new Problem136();
+        int[] nums = new int[] {4, 1, 2, 1, 2};
+        int n = solution.singleNumber(nums);
+        Assert.assertEquals(n, 4);
+
+    }
+}
---
+++ src/main/java/com/leetcode/Problem169.java
@@ -0,0 +1,51 @@
+package com.leetcode;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author kufei.dxm
+ * @date 2022/5/20
+ */
+public class Problem169 {
+    private Problem169 solution;
+
+    @Before
+    public void setUp() {
+        solution = new Problem169();
+    }
+
+    public int majorityElement(int[] nums) {
+        int maj = nums[0];
+        int num = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == maj) {
+                num++;
+            } else {
+                if (num > 0) {
+                    num--;
+                } else {
+                    maj = nums[i];
+                }
+            }
+        }
+        return maj;
+    }
+
+    @Test
+    public void test() {
+        int[] nums = new int[] {3, 2, 3};
+        int maj = solution.majorityElement(nums);
+        assertEquals(maj, 3);
+
+        maj = solution.majorityElement(new int[] {2, 2, 1, 1, 1, 2, 2});
+        assertEquals(maj, 2);
+
+        maj = solution.majorityElement(new int[] {0});
+        assertEquals(0, maj);
+    }
+}
