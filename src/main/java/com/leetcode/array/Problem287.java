d5a502f1d698822d9935b6dc2dc091a56c38b546
kufei <kufei.dxm@alibaba-inc.com>
2022-07-18 11:09:32 +0800

    day day up

---
+++ src/main/java/com/leetcode/array/Problem287.java
@@ -0,0 +1,48 @@
+package com.leetcode.array;
+
+import java.util.BitSet;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 287. Find the Duplicate Number
+ *
+ * @author kufei.dxm
+ * @date 2022/7/18
+ */
+public class Problem287 {
+    private Problem287 solution;
+
+    @Before
+    public void setUp() {
+        solution = new Problem287();
+    }
+
+    /**
+     * 根据题目的限制，配合bitMap可比较直观地求解。（不过不太符合constant space的要求）
+     * 运行性能和时间中位数。
+     * @param nums
+     * @return
+     */
+    public int findDuplicate(int[] nums) {
+        int numCnt = nums.length - 1;
+        BitSet bitSet = new BitSet(numCnt + 1);
+        for (int i : nums) {
+            if (!bitSet.get(i)) {
+                bitSet.set(i);
+            } else {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Test
+    public void test() {
+        int[] nums = new int[] {3,1,3,4,2};
+        int dup = solution.findDuplicate(nums);
+        Assert.assertEquals(dup, 3);
+    }
+}
